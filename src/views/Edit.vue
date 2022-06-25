73b5dc64264291698d0357fe4f476bc25948569f
KalinHar <kalix@abv.bg>
2022-06-25 13:20:59 +0300

    ref edit page

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,5 +1,57 @@
 <template>
-  
+    <div class="container">
+        <div class="columns">
+            <div class="column">
+                <h1 class="title">Редакция на Потребител</h1>
+                <h3>{{client.name}}</h3>
+            </div>
+
+            <div class="column">
+                <form @submit.prevent="submitForm">
+                    <div class="field">
+                        <label>Имена</label>
+                        <div class="control">
+                            <input type="decimal" class="input" v-model="client.name">
+                        </div>
+                    </div>
+
+                    <div class="field">
+                        <label>Телефон</label>
+                        <div class="control">
+                            <input type="text" class="input" v-model="client.phone">
+                        </div>
+                    </div>
+                    
+                    <div class="field">
+                        <label>Показания Старо</label>
+                        <div class="control">
+                            <input type="text" class="input" v-model="client.old">
+                        </div>
+                    </div>
+
+                    <div class="field">
+                        <label>Показания Ново</label>
+                        <div class="control">
+                            <input type="text" class="input" v-model="client.new">
+                        </div>
+                    </div>
+
+                    <div class="field">
+                        <label>Бележка</label>
+                        <div class="control">
+                            <input type="text" class="input" v-model="client.note">
+                        </div>
+                    </div>
+
+                    <div class="field mt-5">
+                        <div class="control">
+                            <button class="button is-success">Запиши</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -34,15 +86,17 @@ export default {
                 .get('/classes/indication?order=-createdAt')
                 .then(response => {
                     this.data = response.data.results[0]
-                    this.objectId = response.data.results[0].objectId
                     this.client = response.data.results[0].units[this.clientID]
-                    console.log(this.data)
                 })
                 .catch(err => {
                     console.log(err)
                 })
 
             this.$store.commit('setLoading', false)
+        },
+        async submitForm() {
+            this.data.units[this.clientID] = this.client
+            console.log(this.data.units)
         }
     }
 }
