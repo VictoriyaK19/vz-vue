9c17d7e77c22c8134c38f8d68c58fe794e1f66e0
KalinHar <kalix@abv.bg>
2022-06-29 07:16:41 +0300

    ref reportin page

--- src/views/Reporting.vue
+++ src/views/Reporting.vue
@@ -17,19 +17,25 @@
                 <tr v-for="cl in indClients" v-bind:key="cl.elN">
                     <td class="has-text-right"><span class="mt-5">{{ cl.name }}</span></td>
                     <td class="has-text-centered">{{ cl.elN }}</td>
-                    <td class="has-text-centered">{{ cl.new }}</td>
+                    <td class="has-text-centered">{{ cl.old }}</td>
                     <td class="has-text-left">
-                        <input type="decimal" class="input is-small" style="width: 50%;" v-model="cl.new">
+                        <input type="number" class="input is-small" style="width: 50%;" v-model="cl.new">
                     </td>
                 </tr>
             </tbody>
         </table>
+    </div>
+
+    <div class="box has-text-centered">
+        <button @click="onSave()" class="button is-info">Запиши показанията</button>
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import { toast } from 'bulma-toast'
+
 
 export default {
     name: 'Reporting',
@@ -48,12 +54,61 @@ export default {
             try {
                 const res = await axios.get('/classes/indication?order=-createdAt&limit=1')
                 this.indClients = Array.from(Object.values(res.data.results[0].units))
+                this.indClients.forEach(cl => {
+                    cl.old = cl.new
+                })
             }
             catch(err) {
                 alert(err.message);
             }
 
             this.$store.commit('setLoading', false)
+        },
+        async onSave() {
+            const toastData = {
+                message: 'info',
+                type: 'is-info',
+                dismissible: true,
+                pauseOnHover: true,
+                duration: 2000,
+                position: 'bottom-right',
+            }
+            const units = this.indClients
+            let err = ''
+            this.indClients.forEach(cl => {
+                if (cl.new < cl.old) {
+                    err = 'Невалидни показания'
+                }
+            })
+
+            if (err) {
+                toastData.message = err
+                toastData.type = 'is-danger'
+                toast(toastData)
+            } else {
+                toastData.message = 'Промените бяха записани успешно.'
+                toastData.type = 'is-success'
+                toast(toastData)
+            }
+
+            // this must replace in "else" - block
+
+            // await axios
+            //     .post('/classes/indications/', {units,})
+            //     .then(response => {
+            //         toastData.message = 'Промените бяха записани успешно.'
+            //         toastData.type = 'is-success'
+            //         toast(toastData)
+
+            //         this.$router.push('/indications')
+            //     })
+            //     .catch(error => {
+            //         console.log(error)
+            //         toastData.message = 'Липсващи или невалидни данни.'
+            //         toastData.type = 'is-danger'
+            //         toast(toastData)
+            //     })
+            
         }
     }
 }
