399aea45ceb473bac365b361c150e1ecb2d6886f
KalinHar <kalix@abv.bg>
2022-06-30 14:33:09 +0300

    test changes for heroku

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="container">
+      <div class="box has-background-info-light">
         <h1 class="has-text-weight-bold">начало</h1>
+      </div>  
     </div>
 </template>
 
