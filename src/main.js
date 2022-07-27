50e37c4bedb95b0d7fb84b41984e90a1d75a3197
KalinHar <kalix@abv.bg>
2022-07-27 10:24:19 +0300

    add config vars on heroku

--- src/main.js
+++ src/main.js
@@ -4,10 +4,24 @@ import router from './router'
 import store from './store'
 import axiios from 'axios'
 
+const aws = require('aws-sdk');
+
+let s3 = new aws.S3({
+  AppId: process.env.APP_ID,
+  AppKey: process.env.APP_KEY
+});
+
 axiios.defaults.baseURL = 'https://parseapi.back4app.com'
 axiios.defaults.headers = {
-    'X-Parse-Application-Id': 'e29amB1TcYqKYQlJDSvtbpHTFnMMdRQiSzEMVDn3',
-    'X-Parse-REST-API-Key': '6rzQoexbCEallVe1wHp0cUXSpNXBSWQgU1LOSq9y'
+    'X-Parse-Application-Id': AppId,
+    'X-Parse-REST-API-Key': AppKey
 }
+
+console.log(AppId)
+console.log(AppKey)
+// axiios.defaults.headers = {
+//     'X-Parse-Application-Id': 'e29amB1TcYqKYQlJDSvtbpHTFnMMdRQiSzEMVDn3',
+//     'X-Parse-REST-API-Key': '6rzQoexbCEallVe1wHp0cUXSpNXBSWQgU1LOSq9y'
+// }
 
 createApp(App).use(store).use(router, axiios).mount('#app')
