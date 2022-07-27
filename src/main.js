e32928c41b96d5e0e2a350205ef862e4bfbc616c
KalinHar <kalix@abv.bg>
2022-07-27 11:15:44 +0300

    ref back

--- src/main.js
+++ src/main.js
@@ -10,19 +10,19 @@ import axiios from 'axios'
 //   AppId: process.env.S3_ID,
 //   AppKey: process.env.S3_KEY
 // });
-var appId = process.env.APP_ID
-var appKey = process.env.APP_KEY
+
+// var appId = process.env.APP_ID
+// var appKey = process.env.APP_KEY
 
 axiios.defaults.baseURL = 'https://parseapi.back4app.com'
-axiios.defaults.headers = {
-    'X-Parse-Application-Id': appId,
-    'X-Parse-REST-API-Key': appKey
-}
-
-console.log(appId)
 // axiios.defaults.headers = {
-//     'X-Parse-Application-Id': 'e29amB1TcYqKYQlJDSvtbpHTFnMMdRQiSzEMVDn3',
-//     'X-Parse-REST-API-Key': '6rzQoexbCEallVe1wHp0cUXSpNXBSWQgU1LOSq9y'
+//     'X-Parse-Application-Id': appId,
+//     'X-Parse-REST-API-Key': appKey
 // }
+
+axiios.defaults.headers = {
+    'X-Parse-Application-Id': 'e29amB1TcYqKYQlJDSvtbpHTFnMMdRQiSzEMVDn3',
+    'X-Parse-REST-API-Key': '6rzQoexbCEallVe1wHp0cUXSpNXBSWQgU1LOSq9y'
+}
 
 createApp(App).use(store).use(router, axiios).mount('#app')
