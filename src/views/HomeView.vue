cbdb4f1585ef78fa93dfba9492836b84164b95d1
KalinHar <kalix@abv.bg>
2022-08-03 12:09:50 +0300

    ref home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="container">
-      <div class="box has-background-info-light">
-        <h1 class="has-text-weight-bold">начало</h1>
-      </div>  
+    <div class="box has-background-info-light">
+        <figure class="image is-3by3">
+            <img src="../assets/vz.png">
+        </figure>
     </div>
 </template>
 
