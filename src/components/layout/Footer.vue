d678705c813d6cdae5d2d7d9ce9c9161e4ca587c
KalinHar <kalix@abv.bg>
2022-08-03 12:23:09 +0300

    add location  on footer

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -7,7 +7,9 @@
                 <a href="https://www.facebook.com/groups/388794204884795">Facebook - Жители на Община Ветово</a>
             </div>
             <div class="column has-text-right">
-                <strong class="column"><a href="#">@KaliX</a></strong>
+                <strong><a href="#">@KaliX</a></strong>
+                <br>
+                <a href="https://www.google.bg/maps/@43.6686215,26.2319194,1176m/data=!3m1!1e3?hl=bg">Location on Google Maps</a>
             </div>
         </div>
     </section>
