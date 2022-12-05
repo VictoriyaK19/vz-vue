616984391ae6b42ed36c29b5510074fef990ccb4
KalinHar <kalix@abv.bg>
2022-12-05 15:15:36 +0200

    ref

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: "/vz-vue/",
+  publicPath: "/vz-vue-version/",
   transpileDependencies: true
 })
